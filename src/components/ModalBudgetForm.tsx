e47247b1ae010007580bb5430b87892e55f6f7dd
Leonardo Vinicio Diaz Avelar <ldiaza2200@alumno.ipn.mx>
2025-11-29 00:01:16 -0600

    agregando modal

--- src/components/ModalBudgetForm.tsx
+++ src/components/ModalBudgetForm.tsx
@@ -1,10 +1,24 @@
 import { PlusIcon } from '@heroicons/react/24/solid'
+import { useBudget } from '../hooks/useBudget'
 
 export default function ModalBudgetForm() {
+
+    const {reducer, dispatch} = useBudget();
+
     return (
         <>
-            <button className="fixed bottom-8 right-8 bg-cyan-950 w-14 h-14 flex justify-center items-center rounded-full">
-                <PlusIcon 
+            <div className={`fixed inset-0 bg-black/75 ${reducer.modal ? 'flex' : 'hidden'} items-center justify-center z-10`}
+                onClick={() => dispatch({type: 'close-modal'})}
+            >
+                <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-lg relative">
+
+                </div>
+            </div>
+
+            <button className="fixed bottom-8 right-8 bg-cyan-950 w-14 h-14 flex justify-center items-center rounded-full"
+                onClick={() => dispatch({type: 'open-modal'})}
+            >
+                <PlusIcon
                     className='text-white w-10'
                 />
             </button>
--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,15 +1,19 @@
 export type BudgetActions =
     { type: 'add-budget', pyload: { budget: number } } |
-    { type: 'valid-budget' }
+    { type: 'valid-budget' } |
+    { type: 'open-modal' } |
+    { type: 'close-modal' } 
 
 export type BudgetStateT = {
     budget: number;
     validBudget: boolean;
+    modal: boolean;
 }
 
 export const initialbudgetState: BudgetStateT = {
     budget: 0,
     validBudget: false,
+    modal: false,
 }
 
 export const budgetReducer = (state: BudgetStateT = initialbudgetState, action: BudgetActions) => {
@@ -23,6 +27,18 @@ export const budgetReducer = (state: BudgetStateT = initialbudgetState, action: 
         return {
             ...state,
             validBudget: true
+        }
+    }
+    if (action.type === "open-modal") {
+        return {
+            ...state,
+            modal: true
+        }
+    }
+    if (action.type === "close-modal") {
+        return {
+            ...state,
+            modal: false
         }
     }
     return state;
